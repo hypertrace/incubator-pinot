851403e13cc8ae0e5097f5dda92cc2814a6fe221
Laxman Ch <60599147+laxmanchekka@users.noreply.github.com>
2024-01-18 16:45:52 +0530

    Ability to use heap buffer for large queries (#386)

--- pinot-segment-local/src/main/java/org/apache/pinot/segment/local/segment/index/readers/forward/ChunkReaderContext.java
+++ pinot-segment-local/src/main/java/org/apache/pinot/segment/local/segment/index/readers/forward/ChunkReaderContext.java
@@ -25,6 +25,8 @@ import java.util.List;
 import org.apache.pinot.segment.spi.index.reader.ForwardIndexReader;
 import org.apache.pinot.segment.spi.index.reader.ForwardIndexReaderContext;
 import org.apache.pinot.segment.spi.memory.CleanerUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 /**
@@ -39,14 +41,31 @@ import org.apache.pinot.segment.spi.memory.CleanerUtil;
  * </ul>
  */
 public class ChunkReaderContext implements ForwardIndexReaderContext {
+  private static final Logger LOGGER = LoggerFactory.getLogger(ChunkReaderContext.class);
+
+  private static final boolean useHeapForLargeChunks;
+  private static final long maxDirectBufferChunkSize;
+  // default max direct buffer threshold size: 2MB
+  private static final long DEFAULT_MAX_DIRECT_BUFFER_CHUNK_SIZE = 2 * 1024 * 1024L;
+
   private final ByteBuffer _chunkBuffer;
 
   private int _chunkId;
 
   private List<ForwardIndexReader.ByteRange> _ranges;
 
+  static {
+    useHeapForLargeChunks = Boolean.parseBoolean(System.getProperty("useHeapForLargeChunks", "false"));
+    maxDirectBufferChunkSize = Long.parseLong(System.getProperty("maxDirectBufferChunkSize", Long.toString(DEFAULT_MAX_DIRECT_BUFFER_CHUNK_SIZE)));
+    LOGGER.info("useHeapForLargeChunks: {}, maxDirectBufferChunkSize: {}", useHeapForLargeChunks, maxDirectBufferChunkSize);
+  }
+
   public ChunkReaderContext(int maxChunkSize) {
-    _chunkBuffer = ByteBuffer.allocateDirect(maxChunkSize);
+    if(!useHeapForLargeChunks || maxChunkSize < maxDirectBufferChunkSize) {
+      _chunkBuffer = ByteBuffer.allocateDirect(maxChunkSize);
+    } else {
+      _chunkBuffer = ByteBuffer.allocate(maxChunkSize);
+    }
     _chunkId = -1;
     _ranges = new ArrayList<>();
   }
@@ -77,5 +96,13 @@ public class ChunkReaderContext implements ForwardIndexReaderContext {
 
   public void setRanges(List<ForwardIndexReader.ByteRange> ranges) {
     _ranges = ranges;
+  }
+
+  @Override
+  public void close()
+      throws IOException {
+    if (CleanerUtil.UNMAP_SUPPORTED && _chunkBuffer.isDirect()) {
+      CleanerUtil.getCleaner().freeBuffer(_chunkBuffer);
+    }
   }
 }
